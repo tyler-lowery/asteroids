4cb739b97ce08e548abc792c148d9e53d7f01425
tyler-lowery <tylerlowery.dev@gmail.com>
2025-07-02 01:34:48 -0500

    Set game to 60 FPS.

--- main.py
+++ main.py
@@ -6,16 +6,17 @@ def main() -> None:
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
         screen.fill(color="black")
         pygame.display.flip()
-    
-    #print("Starting Asteroids!")
-    #print(f"Screen width: {SCREEN_WIDTH}")
-    #print(f"Screen height: {SCREEN_HEIGHT}")
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":

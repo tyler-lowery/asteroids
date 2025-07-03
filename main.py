a6305abfe8a6d3f4aa021d22c5879aff8318e747
tyler-lowery <tylerlowery.dev@gmail.com>
2025-07-03 01:36:48 -0500

    Using pygame Groups instead of individual objects in game loop.

--- main.py
+++ main.py
@@ -9,6 +9,11 @@ def main() -> None:
     clock = pygame.time.Clock()
     dt = 0
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    
     player = Player(x=(SCREEN_WIDTH / 2), y=(SCREEN_HEIGHT / 2))
 
     while True:
@@ -16,10 +21,13 @@ def main() -> None:
             if event.type == pygame.QUIT:
                 return
 
-        player.update(dt)
+        updatable.update(dt)
 
         screen.fill("black")
-        player.draw(screen)
+
+        for obj in drawable:
+            obj.draw(screen)
+        
         pygame.display.flip()
 
         # limit framerate to 60 FPS
